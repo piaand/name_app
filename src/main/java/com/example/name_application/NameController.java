ad3b9a4f358b2767deb26b9e4d50abb13f146634
piaand <pia.m.andersin@gmail.com>
2021-01-13 14:19:34 +0200

    added name/{name} endpoint

--- src/main/java/com/example/name_application/NameController.java
+++ src/main/java/com/example/name_application/NameController.java
@@ -1,11 +1,13 @@
 package com.example.name_application;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * REST API controller. Collection of different end-points user may call.
  * /names/amount - return total amount of all names in the database
+ * /names/{name} - return amount of the name that is give as a path variable
  */
 @RestController
 public class NameController {
@@ -21,5 +23,9 @@ public class NameController {
         return nameService.getTotalAmountOfNames();
     }
 
+    @GetMapping("/names/{name}")
+    public Long getName(@PathVariable String name) {
+        return nameService.getGivenNameAmount(name);
+    }
 
 }
--- src/main/java/com/example/name_application/NameService.java
+++ src/main/java/com/example/name_application/NameService.java
@@ -1,13 +1,17 @@
 package com.example.name_application;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.logging.Logger;
 
 /**
  * NameServices uses NameRepository to fetch the data from database. Contains most of the service logic and CRUD methods.
  */
 @Service
 public class NameService {
-
+    private static final Logger logger = Logger.getLogger(NameApplication.class.getName());
     private final NameRepository nameRepository;
 
     public NameService(NameRepository nameRepository) {
@@ -17,6 +21,19 @@ public class NameService {
     public Long getTotalAmountOfNames() {
         Long result = nameRepository.findTotalAmount() != null ? nameRepository.findTotalAmount() : 0L;
         return result;
+    }
+
+    //Finds the first name that matches the target name or sends responses code 404 if no name found
+    public Long getGivenNameAmount(String targetName) {
+        Name result = nameRepository.findByName(targetName);
+        if (result == null) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "name not found"
+            );
+        } else {
+            Long amount = result.getAmount();
+            return amount;
+        }
     }
 
     public void saveNameFromJson(Name name) {
